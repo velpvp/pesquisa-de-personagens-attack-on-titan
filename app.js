9832d2ef4e80cd2c1c409bdbd2e87572b11112e8
Velp Vp <145732497+velpvp@users.noreply.github.com>
2024-09-06 09:18:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+function pesquisar() {
+
+    let section = document.getElementById('resultados-pesquisa')
+    let campoPesquisa = document.getElementById('campo-pesquisa').value
+    campoPesquisa = campoPesquisa.toLowerCase()
+     let resultados = ''
+    let titulo = ""
+    let descricao = ""
+    let tags = ""
+
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
+                <div class="item-resultado">
+                    <div class="item-resultado-imagem">
+                        <img src="${dado.imagem}" alt="${dado.alt}">
+                    </div>
+                    <div class="item-resultado-infos">
+                        <h2>${dado.titulo}</h2>
+                        <p class="descricao-meta">
+                            ${dado.descricao}
+                        </p>
+                        <a href="${dado.link}" target="_blank">Ver mais...</a>
+                    </div>
+                </div>
+            `;
+        }
+        if (!campoPesquisa || !resultados) {
+            section.innerHTML = `<p>Nada foi encontrado</p>`
+            return;
+        }
+        campoPesquisa = campoPesquisa.toLowerCase()
+    }
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,42 @@
+let dados = [
+    {
+        titulo: "Eren Yeager",
+        imagem: "https://i.pinimg.com/736x/d4/21/45/d42145ae1df350005ca93175fa5c6c62.jpg",
+        alt: "Eren Yeager Icon",
+        descricao: "Eren Yeager é o protagonista de Attack on Titan, um jovem determinado que busca vingar a morte de sua mãe, causada por titãs. Ao longo da série, ele descobre segredos sobre sua própria identidade e o verdadeiro inimigo da humanidade, o que o transforma em uma figura complexa e controversa.",
+        link: "https://attackontitan.fandom.com/pt-br/wiki/Eren_Yeager",
+        tags: "Eren Jaeger, Protagonista, Titã de Ataque"
+    },
+    {
+        titulo: "Armin Arlert",
+        imagem: "https://i.pinimg.com/originals/20/1b/f6/201bf605ca1847fca9cbc6ab5c9378ca.jpg",
+        alt: "Armin Arlert Icon",
+        descricao: "Armin Arlert é um dos personagens principais de Attack on Titan, conhecido por sua inteligência e pensamento estratégico. Amigo de infância de Eren Yeager e Mikasa Ackerman, Armin compensa sua falta de força física com uma mente brilhante, frequentemente encontrando soluções que salvam seus companheiros. Ao longo da série, ele cresce em confiança e coragem, desempenhando um papel crucial na luta pela sobrevivência da humanidade contra os titãs.",
+        link: "https://attackontitan.fandom.com/pt-br/wiki/Armin_Arlert",
+        tags: "Armin Arlert, Estrategista, Amigo de Eren"
+    },
+    {
+        titulo: "Mikasa Ackerman",
+        imagem: "https://i.pinimg.com/originals/64/fd/e7/64fde7832148e36a14b37c968b0d9a22.jpg",
+        alt: "Mikasa Ackerman Icon",
+        descricao: "Mikasa Ackerman é uma das personagens centrais de Attack on Titan, conhecida por sua força, habilidades de combate excepcionais e lealdade inabalável. Crescida ao lado de Eren Yeager após a morte de seus pais, Mikasa se torna uma guerreira feroz, disposta a fazer qualquer coisa para proteger aqueles que ama. Sua ligação profunda com Eren e seu instinto protetor a tornam uma figura tanto poderosa quanto emocionalmente complexa ao longo da série.",
+        link: "https://attackontitan.fandom.com/pt-br/wiki/Mikasa_Ackerman",
+        tags: "Mikasa Ackerman, Guerreira, Protetora"
+    },
+    {
+        titulo: "Hange Zoë",
+        imagem: "https://64.media.tumblr.com/a6594e27b318e0df10f551feb206e0b4/1a85bc47e2c98705-99/s540x810/aebbbec82b206e3fa3819eec573a6055e7975e04.jpg",
+        alt: "Hange Zoë Icon",
+        descricao: "Hange Zoe é uma comandante carismática e excêntrica em Attack on Titan, conhecida por sua curiosidade científica e paixão por entender os titãs. Inicialmente chefe da Divisão de Pesquisa de Titãs, Hange conduz experimentos ousados e sempre busca respostas sobre a origem dessas criaturas. Apesar de seu lado excêntrico, ela também é uma líder estratégica e empática, que assume um papel vital nas decisões críticas da Tropa de Exploração, mostrando tanto inteligência quanto coragem ao longo da série.",
+        link: "https://attackontitan.fandom.com/pt-br/wiki/Hange_Zo%C3%AB",
+        tags: "Hange Zoë, Cientista, Comandante"
+    },
+    {
+        titulo: "Levi Ackerman",
+        imagem: "https://static.wikia.nocookie.net/shingekinokyojin/images/0/0a/Levi_Ackermann_%28Anime%29_character_image_%28854%29.png/revision/latest/scale-to-width/360?cb=20231106070611",
+        alt: "Levi Ackerman Icon",
+        descricao: "Levi Ackerman é um dos personagens mais icônicos de Attack on Titan, conhecido como o soldado mais forte da humanidade. Com habilidades de combate excepcionais e uma personalidade fria e direta, Levi lidera o Esquadrão de Operações Especiais da Tropa de Exploração. Apesar de sua postura rígida e implacável no campo de batalha, ele tem um forte senso de justiça e uma profunda lealdade aos seus companheiros. Sua história revela um passado difícil, moldando-o em um guerreiro que luta incansavelmente pela sobrevivência da humanidade.",
+        link: "https://attackontitan.fandom.com/pt-br/wiki/Levi_Ackerman_(Anime)",
+        tags: "Levi Ackerman, Soldado mais forte, Capitão"
+    }
+];
